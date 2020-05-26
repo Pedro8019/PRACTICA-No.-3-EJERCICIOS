8660996b09eaf893066e97292258246f52f49501
Pedro8019 <65930103+Pedro8019@users.noreply.github.com>
2020-05-25 23:17:27 -0400

    Primer commit

---
+++ 1-) NumerosPares.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumerosPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa que pida al usuario un número entero y diga si es par.
+
+            double num;
+
+            Console.WriteLine("Escriba un número ");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("El número es par");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ 8-) Numeros_Positivos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Numeros_Positivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa que pida al usuario dos números enteros y diga &quot;Uno de los
+            números es positivo&quot;, &quot;Los dos números son positivos&quot; o bien &quot;Ninguno de los
+            números es positivo&quot;, según corresponda.*/
+
+            int n1, n2;
+
+            Console.WriteLine("Digite un número");
+            n1 = Convert.ToSByte(Console.ReadLine());
+
+            Console.WriteLine("Digite otro número");
+            n2 = Convert.ToSByte(Console.ReadLine());
+
+            if ((n1 > 0) && (n2 > 0))
+            {
+                Console.WriteLine("Ambos números son positivos");
+            }
+            else if ((n1 < 0) && (n2 > 0))
+            {
+                Console.WriteLine("Uno de los números es positivo");
+            }
+            else if ((n1 > 0) && (n2 < 0))
+            {
+                Console.WriteLine("Uno de los números es positivo");
+            }
+            else
+            {
+                Console.WriteLine("Ninguno de los números es positivo");
+            }
+            Console.Read();
+        }
+    }
+}
